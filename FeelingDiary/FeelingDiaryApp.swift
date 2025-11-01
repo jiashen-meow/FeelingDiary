9e1cc9709e9e89f69948969d51ad4db0e37bc3e3
Jia Shen <shendaisy0702@gmail.com>
2025-11-01 13:09:59 +0800

    Initial Commit

---
+++ FeelingDiary/FeelingDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelingDiaryApp.swift
+//  FeelingDiary
+//
+//  Created by Jia Shen on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FeelingDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
